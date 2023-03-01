a02460ad31022966e04de0e6ed1c1ddef7a103ac
Jan <jpetko1@gmail.com>
2023-03-01 19:07:12 +0100

    Implemented a hex color app.

--- hex_color_app/js/script.js
+++ hex_color_app/js/script.js
@@ -1,9 +1,28 @@
 // IIFE
 (function(global){
 
+function generateRandomColor() {
+	const letters = '0123456789ABCDEF';
+
+	var color = '#';
+    for (var i = 0; i < 6; i++) {
+    	color += letters[Math.floor(Math.random() * 16)];
+	}
+
+    return color;
+}
+
 // Define all the logic after the content has been loaded.
 document.addEventListener("DOMContentLoaded", function(event){
-	
+
+	document.getElementById("changeColorButton").addEventListener("click", function(){
+		const newColor = generateRandomColor();
+
+		document.body.style.backgroundColor = newColor;
+
+		document.getElementById("hexColorDisplay").innerHTML = newColor;
+
+	});
 });
 
 })(window);
