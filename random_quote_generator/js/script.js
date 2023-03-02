85a47a17d44f49ace4a6e5b439738055d47e8e08
Jan <jpetko1@gmail.com>
2023-03-02 18:27:17 +0100

    Added basic functionality.

--- random_quote_generator/js/script.js
+++ random_quote_generator/js/script.js
@@ -4,6 +4,15 @@
 // Define all the logic after the content has been loaded.
 document.addEventListener("DOMContentLoaded", function(event){
 
+	document.getElementById("generateButton").addEventListener("click", function(){
+
+		const quote = generateRandomQuote();
+		document.getElementById("quoteDiv").innerHTML = quote;
+	});
 });
+
+function generateRandomQuote() {
+	return "Hello World!";
+}
 
 })(window);
