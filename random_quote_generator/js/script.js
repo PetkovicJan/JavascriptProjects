c3999037ffc8d75410412ec9ce6eaf2cf0c25e4e
Jan <jpetko1@gmail.com>
2023-03-02 19:24:11 +0100

    Fetched the quotes from the web.

--- random_quote_generator/js/script.js
+++ random_quote_generator/js/script.js
@@ -6,13 +6,21 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 	document.getElementById("generateButton").addEventListener("click", function(){
 
-		const quote = generateRandomQuote();
-		document.getElementById("quoteDiv").innerHTML = quote;
+		const onGenerate = (result) => {
+			document.getElementById("quoteDiv").innerHTML = result.quote + " " + result.author;
+		};
+
+		generateRandomQuote(onGenerate);
 	});
 });
 
-function generateRandomQuote() {
-	return "Hello World!";
+function generateRandomQuote(onGenerate) {
+	const url = "https://api.breakingbadquotes.xyz/v1/quotes";
+	fetch(url)
+	.then(quote => quote.json())
+	.then(result => {
+		onGenerate(result[0]);
+	});
 }
 
 })(window);
