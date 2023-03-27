6b3b0d25002c7bf98a7a244490bb5a35c979f00e
Jan <jpetko1@gmail.com>
2023-03-27 19:21:47 +0200

    Initialized "grocery list" project and added basic HTML structure.

---
+++ grocery_list/js/script.js
@@ -0,0 +1,15 @@
+// IIFE
+(function(global){
+
+// Define all the logic after the content has been loaded.
+document.addEventListener("DOMContentLoaded", function(event){
+
+  const toGrabList = document.getElementById("toGrabList");
+  const inCartList = document.getElementById("inCartList");
+  document.getElementById("addItemButton").addEventListener("click", () => {
+
+  });
+
+});
+
+})(window);
