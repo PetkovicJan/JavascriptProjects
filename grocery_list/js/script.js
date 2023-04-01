96bbc79d7d7a99187afd63df441668df879715f0
Jan <jpetko1@gmail.com>
2023-04-01 12:26:07 +0200

    Added a function for creating new item elements.

--- grocery_list/js/script.js
+++ grocery_list/js/script.js
@@ -4,12 +4,39 @@
 // Define all the logic after the content has been loaded.
 document.addEventListener("DOMContentLoaded", function(event){
 
+  const itemInput = document.getElementById("item");
+  const quantityInput = document.getElementById("quantity");
+
   const toGrabList = document.getElementById("toGrabList");
   const inCartList = document.getElementById("inCartList");
-  document.getElementById("addItemButton").addEventListener("click", () => {
 
+  document.getElementById("addItemButton").addEventListener("click", () => {
+    const itemName = itemInput.value;
+    const quantity = quantityInput.value;
+
+    addNewItemToList(toGrabList, itemName, quantity);
   });
 
 });
+
+function addNewItemToList(list, name, quantity) {
+
+  const newItem = document.createElement("li");
+  const itemText = document.createTextNode(`${name} (${quantity})`);
+  newItem.appendChild(itemText);
+
+  const addButton = document.createElement("button");
+  const addText = document.createTextNode("Add");
+  addButton.appendChild(addText);
+
+  const removeButton = document.createElement("button");
+  const removeText = document.createTextNode("Remove");
+  removeButton.appendChild(removeText);
+
+  newItem.appendChild(addButton);
+  newItem.appendChild(removeButton);
+
+  list.appendChild(newItem);
+}
 
 })(window);
